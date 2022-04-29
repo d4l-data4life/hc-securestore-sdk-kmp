5912a6dd0932108407c87010339625849d7d0f1a
Matthias Geisler <matthias.geisler@data4life.care>
2022-04-29 11:12:02 +0200

    Bump Android Dependencies

--- gradlePlugin/securestore-dependency/src/main/kotlin/care/data4life/gradle/securestore/dependency/Version.kt
+++ gradlePlugin/securestore-dependency/src/main/kotlin/care/data4life/gradle/securestore/dependency/Version.kt
@@ -75,7 +75,7 @@ object Version {
         /**
          * [mockk](http://mockk.io)
          */
-        const val mockK = "1.10.6"
+        const val mockK = "1.12.3"
     }
 
     object Jvm {
@@ -99,27 +99,27 @@ object Version {
         /**
          * [Android Desugar](https://developer.android.com/studio/write/java8-support)
          */
-        const val androidDesugar = "1.0.9"
+        const val androidDesugar = "1.1.5"
 
         object AndroidX {
             /**
              * [AndroidX](https://developer.android.com/jetpack/androidx)
              */
             const val core = "1.1.0"
-            const val ktx = "1.6.0"
-            const val appCompat = "1.3.1"
-            const val browser = "1.2.0"
+            const val ktx = "1.7.0"
+            const val appCompat = "1.4.1"
+            const val browser = "1.4.0"
 
-            const val constraintLayout = "2.0.1"
+            const val constraintLayout = "2.1.3"
 
-            const val lifecycle = "2.3.1"
-            const val navigation = "2.3.5"
+            const val lifecycle = "2.4.1"
+            const val navigation = "2.4.2"
         }
 
         /**
          * [Material Android](https://github.com/material-components/material-components-android)
          */
-        const val material = "1.4.0"
+        const val material = "1.5.0"
 
         // Crypto
         /**
@@ -139,6 +139,6 @@ object Version {
         /**
          * [Robolectric](https://github.com/robolectric/robolectric)
          */
-        const val robolectric = "4.6.1"
+        const val robolectric = "4.7.3"
     }
 }
