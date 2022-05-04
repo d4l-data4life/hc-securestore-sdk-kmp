75891ce7e36cf78c8fadce787606faade2a7e272
Matthias Geisler <matthias.geisler@data4life.care>
2022-05-04 05:55:21 +0200

    Bump Robolectric

--- gradlePlugin/securestore-dependency/src/main/kotlin/care/data4life/gradle/securestore/dependency/Version.kt
+++ gradlePlugin/securestore-dependency/src/main/kotlin/care/data4life/gradle/securestore/dependency/Version.kt
@@ -139,6 +139,6 @@ object Version {
         /**
          * [Robolectric](https://github.com/robolectric/robolectric)
          */
-        const val robolectric = "4.7.3"
+        const val robolectric = "4.8"
     }
 }
